4d1ebc590b422a0a9e5fba43f4a7c21f783501df
yashivijayvargiya <yashi.vijay24@gmail.com>
2024-06-18 22:57:37 +0530

    Initial commit

---
+++ src/main/java/com/inn/cafe/POJO/User.java
@@ -0,0 +1,116 @@
+package com.inn.cafe.POJO;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@NamedQuery(name="User.findByEmailId",query="select u from User u where u.email=email")
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Data //this create default constructor, getter and setter
+@Table(name="user")
+public class User implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Column(name="id")
+	private Integer id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="contactNumber")
+	private String contactNumber;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="status")
+	private String status;
+	
+	@Column(name="role")
+	private String role;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+
+}
---
+++ src/main/java/com/inn/cafe/constants/CafeConstants.java
@@ -0,0 +1,9 @@
+package com.inn.cafe.constants;
+
+public class CafeConstants {
+	
+	public static final String SOMETHING_WENT_WRONG = "Something went wrong";
+	
+	public static final String INVALID_DATA = "Invalid Data";
+}
+
